42d9117694f8045433028af1146545943c968d8f
Marta Czernik <czernik.m@wp.pl>
2016-05-21 10:36:09 +0200

    commit Portfolio

--- js/app.js
+++ js/app.js
@@ -6,24 +6,23 @@ $(function () {
     var menu = $(".menu");
 
     burger.on("click", function () {
-        menu.toggle();
+        menu.show();
+        burger.hide();
     });
 
-    //Scroll
+    //Scrolling
     var link = $(".menu li a");
 
-    link.on("click", function () { 
-        var position = $($(this).attr("href")).position().top; 
-        $("html, body").animate({ 
-            scrollTop: position 
+    link.on("click", function () {
+        var position = $($(this).attr("href")).position().top;
+        $("html, body").animate({
+            scrollTop: position
         }, 700)
     });
 
     //Menu 
     var nav = $(".navigation");
     var menu_fixed = $(".menu_fixed");
-    console.log(menu_fixed)
-
     var scrollingStartDistance = $(".navigation").offset().top;
 
     $(document).scroll(function () {
@@ -38,33 +37,13 @@ $(function () {
         var span = $(".html p" + $(this).attr('data-width') + ' p');
         span.animate({
             width: $(this).attr('data-width')
-        }, 300);
+        }, 1800);
     })
-
-    //Portfolio 
-
-    var img = $(".box");
-    var inf = $(".modal");
-
-    img.on('click', function () {
-        $(".decor").addClass("add_decor");
-        $(".portfolio").addClass("add");
-        inf.fadeOut(300)
-        var visible = $(this).next().is(':visible')
-        inf.fadeOut(700)
-        if (visible == true) {
-            $(".decor").removeClass("add_decor")
-            $(".portfolio").removeClass("add");
-        } else {
-            $(this).next().fadeIn(700)
-
-        }
-    });
 
     var documentscroll = $('body').scrollTop();
 
     $(window).on('scroll resize', function () {
-        var position = $(".about-my-scills").position().top
+        var position = $(".about-my-skills").position().top
         var documentscroll = $('body').scrollTop();
 
         if (documentscroll === position) {
@@ -79,4 +58,27 @@ $(function () {
             })
         }
     })
+
+    //Portfolio 
+
+    var img = $(".box");
+    var inf = $(".modal");
+
+
+    img.on('click', function () {
+        $(".decor").addClass("add_decor");
+        $(".portfolio").addClass("add");
+
+        inf.fadeOut(300)
+        var visible = $(this).next().is(':visible')
+        inf.fadeOut(700)
+        if (visible == true) {
+            $(".decor").removeClass("add_decor")
+            $(".portfolio").removeClass("add");
+
+        } else {
+            $(this).next().fadeIn(700)
+
+        }
+    });
 })
